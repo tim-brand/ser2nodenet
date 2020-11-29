2bb013f8c535f190497aed0011ad261b412ac610
Tim Brand <t.brand@gmail.com>
2020-11-29 22:33:15 +0100

    added config handler and refactored code for support of multiple ports

--- index.js
+++ index.js
@@ -1,80 +1,24 @@
-const SerialPort = require('serialport')
-const net = require('net')
+const Config = require('./lib/config')
+const PortProxy = require('./lib/port-proxy')
 
-const serialPath = (process.argv.length >= 2) ? process.argv[2] : undefined
-
-if (!serialPath) {
-  console.error('Please define the path to the serialport.')
-  process.exit(1)
-}
-
-const conf = {
-  serial: {
-    path: serialPath,
-    baudRate: 115200
-  }
-}
+const portProxyList = []
 
 const run = async () => {
-  let connectedClientSocket
+  const configPath = process.argv[2]
+  let config = null
 
-  const serialPortOptions = {
-    baudRate: conf.serial.baudRate,
-    rtscts: false,
-    autoOpen: false
+  try {
+    config = await Config.loadFromFile(configPath)
+  } catch (err) {
+    console.error('Error while loading the config file.', err.message)
+    process.exit(1)
   }
-  const serialPort = new SerialPort(conf.serial.path, serialPortOptions, err => {
-    if (err) {
-      console.error('Failed to initiate SerialPort, check your settings.', err)
-      process.exit(1)
-    }
 
-    console.log('SerialPort initiated!')
-  })
-
-  serialPort.on('data', data => {
-    console.log('SerialPort data received:', data.toString())
-    if (connectedClientSocket) {
-      connectedClientSocket.write(data)
-      console.log('Serial data written to the client socket')
-    }
-  })
-
-
-  const socketServer = net.createServer((socket) => {
-    console.debug('Client connected')
-    connectedClientSocket = socket
-
-    socket.on('data', data => {
-      console.debug(`Data received from client: ${data}`)
-      serialPort.write(data, (err, bytesWritten) => {
-        if (err) {
-          // Failed to write to serialport
-          console.error('Failed to write to serialport.', err)
-          return
-        }
-        console.debug(`Data from client written to serialport. bytesWritten: ${bytesWritten}`)
-      })
-    })
-
-    socket.on('close', (hadError) => {
-      if (hadError) {
-        console.warn('Some error occured when client disconnected.')
-      }
-      console.debug('Client disconnected')
-      connectedClientSocket = undefined
-    })
-  })
-  socketServer.listen(2004, '0.0.0.0')
-
-  serialPort.open(async err => {
-    if (err) {
-      console.error('Failed to open SerialPort, check your settings.', err)
-      process.exit(1)
-    }
-
-    console.log('SerialPort opened!')
-  })
+  for(const portConfig of config.ports) {
+    const portProxy = new PortProxy(portConfig)
+    await portProxy.start()
+    portProxyList.push(portProxy)
+  }
 }
 
 run().catch( err => {
